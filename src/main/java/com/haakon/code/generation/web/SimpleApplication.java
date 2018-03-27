b0a4e41ecb6b15bb2955e512f10ac9bc363e2d83
wujh88 <wjh071@gmail.com>
2018-03-27 14:02:05 +0800

    文件修正

--- src/main/java/com/haakon/code/generation/web/SimpleApplication.java
+++ src/main/java/com/haakon/code/generation/web/SimpleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages="com.haakon.code.generation")
 @MapperScan("com.haakon.code.generation.mapper")
 public class SimpleApplication {
-//erer
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
 	}
